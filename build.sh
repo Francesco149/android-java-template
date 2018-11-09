1dd34b7a13f7a9ba461089446f88201d6785fc41
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-09 01:01:20 +0100

    build.sh: show AndroidRuntime errors in the logcat

--- build.sh
+++ build.sh
@@ -118,7 +118,7 @@ EOF
   adb uninstall $namespace || return $?
   adb install "bin/${exename}.apk" || return $?
   adb shell am start -n $namespace/.MainActivity || return $?
-  adb logcat | grep "^./$namespace\..*$" || return $?
+  adb logcat | grep -e "^./$namespace\..*$" -e E/AndroidRuntime || return $?
 }
 
 olddir="$(pwd)"
