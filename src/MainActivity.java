f794e0c17ea8e511f1009f7d41410749f1c8af34
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-09 01:02:57 +0100

    lazy initialization, more readable code

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -8,17 +8,33 @@ import android.widget.TextView;
 import android.view.Gravity;
 
 public class MainActivity extends Activity {
+  private TextView helloTextView = null;
+  private TextView getHelloTextView() {
+    if (helloTextView == null) {
+      TextView x = new TextView(this);
+      x.setGravity(Gravity.CENTER);
+      x.setTextSize(24);
+      x.setText("hello world");
+      helloTextView = x;
+    }
+    return helloTextView;
+  }
+
+  private LinearLayout layout = null;
+  public LinearLayout getLayout() {
+    if (layout == null) {
+      LinearLayout x = new LinearLayout(this);
+      x.setOrientation(LinearLayout.VERTICAL);
+      x.addView(getHelloTextView());
+      layout = x;
+    }
+    return layout;
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    LinearLayout layout = new LinearLayout(this);
-    layout.setOrientation(LinearLayout.VERTICAL);
-    TextView helloTextView = new TextView(this);
-    helloTextView.setGravity(Gravity.CENTER);
-    helloTextView.setTextSize(24);
-    helloTextView.setText("hello world");
-    layout.addView(helloTextView);
-    setContentView(layout);
+    setContentView(getLayout());
     Log.d(getClass().getName(), "hello world");
   }
 }
